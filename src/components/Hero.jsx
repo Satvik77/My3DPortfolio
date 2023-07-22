d58129e4eadcc60edea64b4385ee37239182c389
Satvik77 <satviksharma124@gmail.com>
2023-07-23 05:21:30 +0530

    Fixed build issue

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I am a CSE Graduate'23 <br className='sm:block hidden' />
-            and a SDE enthusiast
+            and a SDE Fresher
           </p>
         </div>
       </div>
